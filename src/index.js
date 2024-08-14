e9dce83f4407782f61f4c2b63303c4e6ed440044
Flaxid Hair <kmarlou@icloud.com>
2024-08-14 10:42:02 +0300

    feat: end of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return parseInt(n.toString().split("").reverse().join(""));
+};
+
+// function a(n) {
+//     return console.log(Number(toString(n).split("").reverse().join("")));
+// }
+
+// a(123123);
